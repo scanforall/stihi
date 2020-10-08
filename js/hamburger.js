aff64df420593430674593c7cec49d0b7aa834df
Vasiliy Tikhonov <scanforall@mail.ru>
2020-10-08 22:40:55 +0300

    fix hidden menu

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,3 +1,4 @@
 document.querySelector('.hamburger').onclick = function(){
   document.querySelector('.hamburger-icon').classList.toggle('hamburger-icon-active');
+  document.querySelector('.menu-hidden__list').classList.toggle('menu-hidden__list-active');
 }
