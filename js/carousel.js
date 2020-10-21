7c8ce4d7a4647bcf3f21fbf05058f3e257af81b1
Vasiliy Tikhonov <scanforall@mail.ru>
2020-10-21 21:00:58 +0300

    fix carousel

---
+++ js/carousel.js
@@ -0,0 +1,10 @@
+const slider = $('.carousel__slider').bxSlider({
+  mode: 'horizontal',
+  autoDirection: 'next',
+  minSlides: 8,
+  maxSlides: 8,
+  slideWidth: 100,
+  // slideMargin: 8,
+  ticker: true,
+  speed: 50000
+});
