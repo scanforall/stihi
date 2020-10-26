d7907c7f5e4424c13b89db2a7adfc3d5673b5870
Vasiliy Tikhonov <scanforall@mail.ru>
2020-10-26 14:38:15 +0300

    fix

---
+++ js/slider-prev.js
@@ -0,0 +1,11 @@
+const sliderPrev = $('.preview__slider').bxSlider({
+  // mode: 'horizontal',
+    pager: true,
+		controls: false,
+		auto: true,
+		pause: 10000,
+		minSlides: 3,
+		maxSlides: 3,
+		slideMargin: 20,        
+		slideWidth: 236
+});
